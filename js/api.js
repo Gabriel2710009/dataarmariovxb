5df71205d85ae078e0e62553b8bd273663ff820e
Gabriel2710009 <lozanoarcegabriel@gmail.com>
2025-12-20 06:53:21 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,16 @@
 async function cargarArmas() {
-  const res = await fetch("https://backend-pefa.onrender.com/api/registro_armas", {
-    credentials: "include"
-  });
+  const res = await fetch(
+    "https://backend-pefa.onrender.com/api/registro_armas",
+    {
+      credentials: "include"
+    }
+  );
+
+  if (!res.ok) {
+    console.log("No autorizado");
+    return;
+  }
+
   const data = await res.json();
+  renderTabla(data);
 }
