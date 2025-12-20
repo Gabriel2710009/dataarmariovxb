8419fa6712a3cc74d2f529b23931c61bd3bdd76c
Gabriel2710009 <lozanoarcegabriel@gmail.com>
2025-12-20 06:39:00 -0300

    Delete js directory

--- js/auth.js
+++
@@ -1,7 +0,0 @@
-const API = "https://backend-pefa.onrender.com";
-
-const btnLogin = document.getElementById("login");
-
-btnLogin.addEventListener("click", () => {
-  window.location.href = `${API}/auth/discord`;
-});
--- js/theme.js
+++
@@ -1,4 +0,0 @@
-function toggleTheme() {
-  const actual = document.body.dataset.theme;
-  document.body.dataset.theme = actual === "light" ? "dark" : "light";
-}
